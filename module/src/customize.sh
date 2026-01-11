50503dc7ace257099c99cd3ac7a405777501e332
知秋 <127842603+947409161@users.noreply.github.com>
2026-01-11 17:47:01 +0800

    更新 customize.sh

--- module/src/customize.sh
+++ module/src/customize.sh
@@ -202,3 +202,4 @@ if [ "$HUAWEI_MAPLE_ENABLED" == "1" ]; then
   ui_print "- Add ro.maple.enable=0"
   echo "ro.maple.enable=0" >>"$MODPATH/system.prop"
 fi
+cp "$MODPATH/module.prop" "$MODPATH/module.prop.bak"
